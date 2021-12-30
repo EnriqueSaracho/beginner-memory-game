edcf333aea09fa94ef0db5ae14e5ed227bc6358b
Enrique Saracho Felix <esarachof97@gmail.com>
2021-12-30 12:12:45 -0700

    Update app.js
    Added card options

---
+++ app.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+    ]
+
+    
+})
